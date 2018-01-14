e6aff339e943e30e37067ecb92b40c002c88c94d
Jacob Davis <contact@1forge.com>
2018-01-14 11:58:34 -0800

    Update ExampleUsage.rb

--- ExampleUsage.rb
+++ ExampleUsage.rb
@@ -1,3 +1,8 @@
+=begin
+  This library is provided without warranty under the MIT license
+  Created by Jacob Davis <jacob@1forge.com>
+=end
+
 require 'forex_quotes'
 
 client = ForexDataClient.new('vJHZqgJf7V3tvd7BA3MGThaB3NqVX7FD')
